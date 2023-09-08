81adf79370bedeb51ce204494392dffe78543df2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-08 20:26:19 +0300

    Added some more configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: 'jest-environment-js-dom'
+	testEnvironment: 'jest-environment-js-dom',
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+	}
 }
